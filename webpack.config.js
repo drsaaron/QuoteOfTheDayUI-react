0ef5532fe74794a221332facb0899edde8f8b6f5
Scott Aaron <dr_saaron@yahoo.com>
2019-01-05 08:17:35 -0600

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 	extensions: ['.js', '.jsx']
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /.jsx?$/,
                 loader: 'babel-loader',
