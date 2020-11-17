00816edbde90ce7e9ffb6c2299ba12025317ceeb
Scott Aaron <dr_saaron@yahoo.com>
2020-11-17 10:42:55 -0600

    update versions

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
-docker build -t quoteofthedayui-react  .
+version=$(perl -MJSON -ne 'BEGIN { $/ = undef; } my $json = from_json($_); print $json->{version} . "\n";' package.json)
+
+imageName=quoteofthedayui-react
+
+docker build -t $imageName  .
+docker tag $imageName $imageName:$version
