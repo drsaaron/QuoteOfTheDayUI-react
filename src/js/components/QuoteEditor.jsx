37e87e763ba06f979c5acc9980c45d97c2cf3c46
Scott Aaron <dr_saaron@yahoo.com>
2022-01-04 15:05:15 -0600

    removed unused imports

--- src/js/components/QuoteEditor.jsx
+++ src/js/components/QuoteEditor.jsx
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-import {useState, useEffect} from 'react';
 import { connect } from 'react-redux';
 import { quoteDataUpdate } from '../actions/QuoteActions';
 
