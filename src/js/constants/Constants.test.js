07f301171858e2a3b4fa8b1bdafe9be0438d0cf2
Scott Aaron <dr_saaron@yahoo.com>
2022-03-21 14:36:06 -0500

    change the DATA_URL_ROOT to reflect the call back to the server rather than the data service

--- src/js/constants/Constants.test.js
+++ src/js/constants/Constants.test.js
@@ -1,6 +1,6 @@
 import {DATA_URL_ROOT} from './Constants';
 
 test('dev environment', () => {
-    expect(DATA_URL_ROOT).toBe('http://localhost:' + 30000);
+    expect(DATA_URL_ROOT).toBe('/api');
 });
 
