21856172ae6c6c0fc4d39071940af0247e855d32
Scott Aaron <dr_saaron@yahoo.com>
2022-04-06 16:57:28 -0500

    convert QuoteOfTheDay to function

--- src/js/components/QuoteOfTheDay.js
+++ src/js/components/QuoteOfTheDay.js
@@ -4,60 +4,49 @@
  * and open the template in the editor.
  */
 
-import React, {Component} from 'react';
+import {useState} from 'react';
 import QuoteText from './QuoteText';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-export default class QuoteOfTheDay extends Component {
-    constructor(props) {
-        super(props);
+const QuoteOfTheDay = (props) => {
 
-        this.state = {
-            runDate: this.props.quoteOfTheDay.runDate,
-            maxDate: new Date()
-        };
+    const [runDate, setRunDate] = useState(props.quoteOfTheDay.runDate);
+    const maxDate = new Date();
 
-        this.handleSourceClick = this.handleSourceClick.bind(this);
-        this.handleDateChange = this.handleDateChange.bind(this);
-        this.showDetails = this.showDetails.bind(this);
-        
-    }
-    
-    handleSourceClick(event) {
-        console.log("getting quotes for " + this.props.quoteOfTheDay.sourceCode.number);
+    const handleSourceClick = (event) => {
+        console.log("getting quotes for " + props.quoteOfTheDay.sourceCode.number);
 
         event.preventDefault();
-        this.props.retrieveQuotesForSourceCode(this.props.quoteOfTheDay.sourceCode);
+        props.retrieveQuotesForSourceCode(props.quoteOfTheDay.sourceCode);
     }
     
-    showDetails(event) {
+    const showDetails = (event) => {
         event.preventDefault();
-        this.props.showDetails(this.props.quoteOfTheDay.qotd.quoteNumber, this.props.navigate);
+        props.showDetails(props.quoteOfTheDay.qotd.quoteNumber, props.navigate);
     }
     
-    handleDateChange(date) {
-        this.setState({ runDate: date });
-        this.getQuoteOfTheDay(date);
+    const handleDateChange = (date) => {
+	setRunDate(date);
+        getQuoteOfTheDay(date);
     }
 
-    render() {
-        var quoteNumber = this.props.quoteOfTheDay.qotd.quoteNumber || -1;
-    
-        return (
-                <div id="quoteOfTheDay">
-                    <h2>Quote of the day</h2>
-                    <DatePicker maxDate={this.state.maxDate} selected={this.state.runDate} onChange={this.handleDateChange} />
-                    Quote #<a href="/" onClick={this.showDetails}>{quoteNumber}</a>
-                    <QuoteText quote={this.props.quoteOfTheDay.quote} />
-                    <div className="QuoteSource">Source: <em><a href="/" onClick={this.handleSourceClick}>{this.props.quoteOfTheDay.sourceCode.text}</a></em></div>
-                </div>
-                );
-    }
-
-    getQuoteOfTheDay(runDate) {
+    const getQuoteOfTheDay = (runDate) => {
         console.log("getting quote of the day for " + runDate);
-        this.props.retrieveQuoteOfTheDay(runDate);
+        props.retrieveQuoteOfTheDay(runDate);
     }
+
+    var quoteNumber = props.quoteOfTheDay.qotd.quoteNumber || -1;
     
+    return (
+        <div id="quoteOfTheDay">
+            <h2>Quote of the day</h2>
+            <DatePicker maxDate={maxDate} selected={runDate} onChange={handleDateChange} />
+            Quote #<a href="/" onClick={showDetails}>{quoteNumber}</a>
+            <QuoteText quote={props.quoteOfTheDay.quote} />
+            <div className="QuoteSource">Source: <em><a href="/" onClick={handleSourceClick}>{props.quoteOfTheDay.sourceCode.text}</a></em></div>
+        </div>
+    );
 }
+
+export default QuoteOfTheDay;
