beee371ebc933d23406e78bd7d704011035044d4
Scott Aaron <dr_saaron@yahoo.com>
2022-04-06 16:40:45 -0500

    add unit test for QuoteOfTheDay

---
+++ src/js/components/QuoteOfTheDay.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import QuoteOfTheDay from './QuoteOfTheDay';
+
+describe('header', () => {
+    test('basic', () => {
+	const qotd = {
+	    runDate: 1000,
+	    sourceCode: {
+		number: 10,
+		text: 'test source'
+	    },
+	    quote: {
+		number: 5,
+		text: 'I am a quote'
+	    },
+	    qotd: {
+		quoteNumber: 5
+	    }
+	};
+	const component = renderer.create(<QuoteOfTheDay quoteOfTheDay={qotd} />);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+    });
+});
