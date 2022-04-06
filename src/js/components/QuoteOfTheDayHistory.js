e8ec24d81dfa9db2ea85183b3c3560af688d4a74
Scott Aaron <dr_saaron@yahoo.com>
2022-04-06 17:05:20 -0500

    convert QuoteOfTheDayHistory to function

--- src/js/components/QuoteOfTheDayHistory.js
+++ src/js/components/QuoteOfTheDayHistory.js
@@ -4,21 +4,20 @@
  * and open the template in the editor.
  */
 
-import React, {Component} from 'react';
 import QuoteOfTheDayHistoryForYear from './QuoteOfTheDayHistoryForYear';
 
-export default class QuoteOfTheDayHistory extends Component {
+const QuoteOfTheDayHistory = (props) => {
 
-    render() {
-        var years = Object.keys(this.props.history);
-        var noHistory = years.length === 0 ? <em>No history</em> : "";
-        return (
-                <div id="quoteOfTheDayHistory">
-                    <h2>Quote of the day history</h2>
-                    {noHistory}
-                    { years.map(year => <QuoteOfTheDayHistoryForYear key={year} year={year} history={this.props.history[year]} />)}
-                </div>
-                );
-    }
+    var years = Object.keys(props.history);
+    var noHistory = years.length === 0 ? <em>No history</em> : "";
+    return (
+        <div id="quoteOfTheDayHistory">
+            <h2>Quote of the day history</h2>
+            {noHistory}
+            { years.map(year => <QuoteOfTheDayHistoryForYear key={year} year={year} history={props.history[year]} />)}
+        </div>
+    );
 }
+
+export default QuoteOfTheDayHistory;
 
