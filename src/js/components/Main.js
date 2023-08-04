5140c14bc6391a35c286a9685201790274032a44
Scott Aaron <dr_saaron@yahoo.com>
2023-08-04 14:20:00 -0500

    add the polling logic to automatically logout when the token expires

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import { useEffect } from 'react';
 import Header from './Header';
 import SourceCodeList from './SourceCodeList';
 import QuotesForSourceCodeList from './QuotesForSourceCodeList';
@@ -13,6 +14,7 @@ import { retrieveQuotesForSourceCode } from '../actions/SourceCodeActions';
 import { showQuoteDetails, addQuote, addSourceCode, goHome } from '../actions/NavigationActions';
 import { retrieveQuoteOfTheDay } from '../actions/QuoteOfTheDayActions';
 import { useNavigate } from 'react-router-dom';
+import { loginForm, logout, updateTokenMonitorInterval } from '../actions/LoginActions';
 
 const {version} = require("../../../package.json");
 
@@ -32,12 +34,55 @@ const mapDispatchToProps = (dispatch) => {
         showQuoteDetails: (quoteNumber, navigate) => dispatch(showQuoteDetails(quoteNumber, navigate)),
         addQuote: (sourceCode, navigate) => dispatch(addQuote(sourceCode.number, navigate)),
 	addSourceCode: (navigate) => dispatch(addSourceCode(navigate)),
-	goHome: (navigate) => dispatch(goHome(navigate))
+	goHome: (navigate) => dispatch(goHome(navigate)),
+	loginForm: (navigate) => loginForm(navigate),
+	logout: () => dispatch(logout()),
+	updateTokenMonitorInterval: (intervalId) => dispatch(updateTokenMonitorInterval(intervalId))
     };
 };
 
 const Main = (props) => {
 
+    /* if the JWT token is define, i.e. user is logged in, periodically check for expiration.
+       when expired, logout. Storing the interval ID in state (via useState) does not seem to work
+       when navigating to child pages and back.  Store store in teh redux store. */
+    const checkTokenExpiration = (token) => {
+	var parsedToken = JSON.parse(atob(token.split('.')[1]));
+	var duration = parsedToken.exp * 1000 - Date.now();
+	if (parsedToken.exp * 1000 < Date.now()) {
+	    console.log("token expired, logging out");
+	    props.logout();
+	}
+    };
+    
+    const startJwtPolling = () => {
+	var jwtToken = props.login.token;
+	var intervalId = props.login.tokenMonitorInterval;
+	if (intervalId < 0 && jwtToken !== null) {
+	    var i = setInterval(() => checkTokenExpiration(jwtToken), 60000);
+	    props.updateTokenMonitorInterval(i);
+	}
+    };
+
+    const stopJwtPolling = () => {
+	var intervalId = props.login.tokenMonitorInterval;
+	if (intervalId > 0) {
+	    clearInterval(intervalId);
+	    props.updateTokenMonitorInterval(-1);
+	}
+    };
+    
+    // whenever the JWT token is null, navigate to login
+    useEffect(() => {
+	var jwtToken = props.login.token;
+	startJwtPolling();
+	if (jwtToken == null) {
+	    console.log("no token, going to login page");
+	    props.loginForm(navigate);
+	}
+	return stopJwtPolling;
+    }, [props.login.token]);
+    
     const navigate = useNavigate();
     console.log("token = " + props.login.token);
     
