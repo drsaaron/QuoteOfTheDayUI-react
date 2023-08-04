3907c509e25f19451335be1ed50da531c07037a0
Scott Aaron <dr_saaron@yahoo.com>
2023-08-04 13:21:42 -0500

    authenticate seems to work

--- src/js/reducers/LoginReducer.js
+++ src/js/reducers/LoginReducer.js
@@ -14,30 +14,33 @@ const initialState = {
 
 export default function loginReducer(state = initialState, action) {
     switch (action.type) {
-        case ActionTypes.USER_LOGGED_IN:
-            return {
-                ...state,
-                user: action.user,
-                token: action.token,
-                errorMessage: null
-            };
-
-        case ActionTypes.USER_LOGGED_OUT:
-            return {
-                ...state,
-                token: null
-            };
-
-        case ActionTypes.USER_LOGIN_FAILURE:
-            return {
-                ...state,
-                errorMessage: action.errorMessage
-            };
-
-        case ActionTypes.TOKEN_MONITOR_INTERVAL_ID_UPDATE:
-            return {
-                ...state,
-                tokenMonitorInterval: action.intervalId
-            };
-}
+    case ActionTypes.USER_LOGGED_IN:
+        return {
+            ...state,
+            user: action.user,
+            token: action.token,
+            errorMessage: null
+        };
+	
+    case ActionTypes.USER_LOGGED_OUT:
+        return {
+            ...state,
+            token: null
+        };
+	
+    case ActionTypes.USER_LOGIN_FAILURE:
+        return {
+            ...state,
+            errorMessage: action.errorMessage
+        };
+	
+    case ActionTypes.TOKEN_MONITOR_INTERVAL_ID_UPDATE:
+        return {
+            ...state,
+            tokenMonitorInterval: action.intervalId
+        };
+	
+    default:
+	return state;
+    }
 };
