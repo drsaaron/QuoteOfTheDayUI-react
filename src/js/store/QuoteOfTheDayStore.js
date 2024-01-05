aa5704834c074b1f76f756c00c37a725970eea12
Scott Aaron <dr_saaron@yahoo.com>
2024-01-05 11:42:18 -0600

    version update

--- src/js/store/QuoteOfTheDayStore.js
+++ src/js/store/QuoteOfTheDayStore.js
@@ -11,7 +11,7 @@ import QuotesForSourceCodeService from '../services/QuotesForSourceCodeService';
 import QuoteService from '../services/QuoteService';
 import QuoteOfTheDayService from '../services/QuoteOfTheDayService';
 import { createBrowserHistory } from 'history';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 // Create a history of your choosing (we're using a browser history in this case)
 export const history = createBrowserHistory();
