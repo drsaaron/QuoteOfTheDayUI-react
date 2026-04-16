c55809f2ee2e7eb28b83b3ae09733884939f8995
Scott Aaron <dr_saaron@yahoo.com>
2026-04-16 13:10:15 -0500

    version update

--- src/js/api/QuoteOfTheDayAPI.js
+++ src/js/api/QuoteOfTheDayAPI.js
@@ -7,13 +7,23 @@
 import {authenticatedGet} from '../actions/AuthenticatedRequest';
 import { DATA_URL_ROOT } from '../constants/Constants';
 
+function formatDateWithPadding(dateStr) {
+  const [year, month, day] = dateStr.split('-');
+  
+  // padStart(2, '0') ensures the string is at least 2 characters long
+  const paddedMonth = month.padStart(2, '0');
+  const paddedDay = day.padStart(2, '0');
+  
+  return `${year}-${paddedMonth}-${paddedDay}`;
+}
+
 export default class QuoteOfTheDayAPI {
     static getQuoteOfTheDayHistory(quoteNumber, token) {
         return authenticatedGet(DATA_URL_ROOT + "/qotdHistory/" + quoteNumber, token);
     }
 
     static getQuoteOfTheDay(runDate, token) {
-        return authenticatedGet(DATA_URL_ROOT + "/qotd/" + runDate, token);
+        return authenticatedGet(DATA_URL_ROOT + "/qotd/" + formatDateWithPadding(runDate), token);
     }
 }
 
